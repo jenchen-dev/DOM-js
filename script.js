0b934c98aabf64ae3e3eda83a387b911c02d2f3f
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-05-08 00:29:48 +0800

    add more functions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var delbutton = document.querySelectorAll(".delete");
+var reset = document.querySelector(".reset");
 
 function inputLength() {
     return input.value.length;
@@ -25,6 +27,32 @@ function addListAfterKeydown(Event) {
     }
 }
 
-//callback function
-button.addEventListener("click", addListAfterClick)
-input.addEventListener("keydown", addListAfterKeydown)
+function toggleListItem(element) {
+    if (element.target.tagName === "LI") {
+        element.target.classList.toggle("done");
+    }
+}
+
+function deleteListElement() {
+    var items = document.querySelectorAll(".li-wrapper");
+    for (var ind = 0; ind < items.length; ind++) {
+        this.parentNode.remove();
+    }
+}
+
+function deleteListAfterClick() {
+    for (var i = 0; i < delbutton.length; i++) {
+        delbutton[i].addEventListener("click", deleteListElement);
+    }
+}
+
+function deleteAllList() {
+    ul.innerHTML = "";
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keydown", addListAfterKeydown);
+ul.addEventListener("click", toggleListItem);
+reset.addEventListener("click", deleteAllList);
+
+deleteListAfterClick();
